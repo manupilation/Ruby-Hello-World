1d05fa3ff4919c538795141f3c331d4af48bbd79
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-05-31 19:57:31 -0300

    feat: ruby stdin and stdout

---
+++ modulo/stdinStdout.rb
@@ -0,0 +1,20 @@
+puts "Digite seu nome:"
+nome = gets.chomp
+puts "O seu nome é: " + nome
+
+puts "======================="
+
+puts "Digite sua idade: "
+idade = gets.to_i
+puts "A sua idade é: " + idade
+
+
+puts "======================="
+
+
+puts "Digite seu salário: "
+salario = gets.to_f
+puts "Seu novo salário é: " + (salario * 1.10).to_s
+
+puts "======================="
+
